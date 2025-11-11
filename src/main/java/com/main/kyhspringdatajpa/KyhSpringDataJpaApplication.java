c62aca5794422ad2659f869d1e056b591945cc80
KimYoonMin <dbsalszz@naver.com>
2025-11-11 16:51:11 +0900

    initial setting

---
+++ src/main/java/com/main/kyhspringdatajpa/KyhSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.main.kyhspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhSpringDataJpaApplication.class, args);
+    }
+
+}
