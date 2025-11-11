e2de93accc4ec274d4e6108dad74120400952db1
KimYoonMin <dbsalszz@naver.com>
2025-11-11 22:16:13 +0900

    JPA 활용 Member repository 추가

---
+++ src/main/java/com/main/kyhspringdatajpa/repository/MemberJpaRepository.java
@@ -0,0 +1,23 @@
+package com.main.kyhspringdatajpa.repository;
+
+import com.main.kyhspringdatajpa.entity.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberJpaRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+
+    }
+}
