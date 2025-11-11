65658018617fdf6d9230ac4b8e64aa82a423ab6f
KimYoonMin <dbsalszz@naver.com>
2025-11-11 22:15:52 +0900

    Member 엔티티 추가

---
+++ src/main/java/com/main/kyhspringdatajpa/entity/Member.java
@@ -0,0 +1,30 @@
+package com.main.kyhspringdatajpa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+public class Member {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    protected Member() {
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public void changeUsername(String username) {
+        this.username = username;
+    }
+
+
+}
