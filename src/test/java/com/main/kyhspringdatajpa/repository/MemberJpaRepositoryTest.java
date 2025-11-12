82287b68ac850f079921f57e093cef442f3ad445
KimYoonMin <dbsalszz@naver.com>
2025-11-12 17:50:54 +0900

    Member Jpa 레포지토리 테스트 수정

--- src/test/java/com/main/kyhspringdatajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/com/main/kyhspringdatajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -28,5 +30,34 @@ class MemberJpaRepositoryTest {
 
     @Test
     void find() {
+    }
+
+    @Test
+    public void basicCRUD() {
+
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member foundMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        //then
+        assertEquals(foundMember1.getId(), member1.getId());
+        assertEquals(foundMember2.getId(), member2.getId());
+
+        List<Member> all = memberJpaRepository.findALl();
+        assertEquals(2, all.size());
+
+        long count = memberJpaRepository.count();
+        assertEquals(2, count);
+
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertEquals(0, deletedCount);
     }
 }
