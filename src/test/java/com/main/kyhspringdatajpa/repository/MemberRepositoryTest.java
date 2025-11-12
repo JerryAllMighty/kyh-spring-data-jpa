aae5b2b93d64274843d3246ca7df9b1c8942c45d
KimYoonMin <dbsalszz@naver.com>
2025-11-12 21:40:34 +0900

    Member Repository 수정

--- src/test/java/com/main/kyhspringdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/main/kyhspringdatajpa/repository/MemberRepositoryTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
