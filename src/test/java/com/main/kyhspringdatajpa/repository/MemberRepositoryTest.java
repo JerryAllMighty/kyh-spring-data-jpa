423a0928200c671f4e4b8480fcf61f75d1b96bdd
KimYoonMin <dbsalszz@naver.com>
2025-11-12 19:30:21 +0900

    SPRING DATE JPA 활용 메서드 테스트 추가 : 사용자명과 나이로 회원 검색

--- src/test/java/com/main/kyhspringdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/com/main/kyhspringdatajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,6 +29,51 @@ class MemberRepositoryTest {
         Assertions.assertEquals(foundMember.getId(), member.getId());
         Assertions.assertEquals(foundMember.getUsername(), member.getUsername());
         Assertions.assertEquals(foundMember, member);
+    }
+
+    @Test
+    public void basicCRUD() {
+
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member foundMember1 = memberRepository.findById(member1.getId()).get();
+        Member foundMember2 = memberRepository.findById(member2.getId()).get();
+
+        //then
+        assertEquals(foundMember1.getId(), member1.getId());
+        assertEquals(foundMember2.getId(), member2.getId());
+
+        List<Member> all = memberRepository.findAll();
+        assertEquals(2, all.size());
+
+        long count = memberRepository.count();
+        assertEquals(2, count);
+
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertEquals(0, deletedCount);
+    }
+
+    @Test
+    public void findByUserNameAndAgeGreaterThan() {
+        //given
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("AAA", 20);
+
+        //when
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        //then
+        List<Member> members = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 10);
+        assertEquals(1, members.size());
     }
 
 
