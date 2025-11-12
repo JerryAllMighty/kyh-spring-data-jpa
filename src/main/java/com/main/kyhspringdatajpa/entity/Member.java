a4ac492767145fa310a3685af7a8551f1d5f6e0e
KimYoonMin <dbsalszz@naver.com>
2025-11-12 17:50:31 +0900

    Member 엔티티 수정

--- src/main/java/com/main/kyhspringdatajpa/entity/Member.java
+++ src/main/java/com/main/kyhspringdatajpa/entity/Member.java
@@ -1,29 +1,45 @@
 package com.main.kyhspringdatajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
-    protected Member() {
-    }
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
     public void changeUsername(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 
 
