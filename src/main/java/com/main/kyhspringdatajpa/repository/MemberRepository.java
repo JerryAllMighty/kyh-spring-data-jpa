c3b688ed868acf877322e2e30a6cbce30bde2b95
KimYoonMin <dbsalszz@naver.com>
2025-11-13 16:10:38 +0900

    Spring data JPA  활용 : 다양한 리턴타입유형의 메서드 추가

--- src/main/java/com/main/kyhspringdatajpa/repository/MemberRepository.java
+++ src/main/java/com/main/kyhspringdatajpa/repository/MemberRepository.java
@@ -1,11 +1,34 @@
 package com.main.kyhspringdatajpa.repository;
 
+import com.main.kyhspringdatajpa.dto.MemberDto;
 import com.main.kyhspringdatajpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.main.kyhspringdatajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+
+    @Query("select m from Member  m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);
+
+
+    List<Member> findListByUsername(String username);
+
+    Member findMemberByUsername(String username);
+    Optional<Member> findOptionalByUsername(String username);
+
 }
